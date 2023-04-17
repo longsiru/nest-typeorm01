7143c5ecfbf5924c793f65f3cf0e9451e11dc734
longsiru <sirulong89@gmail.com>
2023-04-18 01:36:48 +0900

    Update news.controller.ts

--- src/module/admin/controller/news/news.controller.ts
+++ src/module/admin/controller/news/news.controller.ts
@@ -9,7 +9,7 @@ export class NewsController {
 
   @Get()
   async index() {
-    console.log(await this.articleService.findAll());
+    console.log(await this.articleService.findAll()); //返回空，但是我们可以去mysql看table，可以看到生成了article table
     return '我是admin模块里面的news控制器里面的方法';
   }
 }
