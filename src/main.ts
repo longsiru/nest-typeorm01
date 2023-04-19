7c6c55bb15d9a1d41406f6cc210c204059049a0c
longsiru <sirulong89@gmail.com>
2023-04-19 23:31:08 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 }
 bootstrap();
 
-//1. npm install --save @nestjs/typeorm typeorm mysql
+//1. npm install --save @nestjs/typeorm typeorm mysql2
 //2. 配置数据库连接。 app.module
 //3. 配置实体 entity，在src里面新建一个文件夹，名字叫做entity，然后新建操作数据的实体。然后在要操作的contrller对应的module配置model
 //4. 定义service来获取module，来操作数据库。
